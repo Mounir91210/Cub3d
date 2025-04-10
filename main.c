a3f1eb0f702efac1166416264d43d2fb4b94e175
mounir <modavid@student.42.fr>
2025-04-10 21:55:54 +0200

    taketexture

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: modavid <modavid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 18:54:35 by modavid           #+#    #+#             */
-/*   Updated: 2025/04/08 21:11:59 by modavid          ###   ########.fr       */
+/*   Updated: 2025/04/10 21:51:35 by modavid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,39 @@ int	check_extension(char *extension)
 	if (ft_strcmp(ft_strrchr(extension, '.'), ".cub") == 0)
 		return (0);
 	return (1);
+}
+
+int	check_no_texture(char *str)
+{
+	int	i;
+
+	i = 0;
+	check_space(str, &i);
+	if (str[i] != 'N' || str[i] != 'S' || str[i] != 'W'
+		|| str[i] != 'E' || str[i] != 'F' || str[i] != 'C')
+		return (1);
+	return (0);
+}
+
+char	**take_texture(int fd)
+{
+	char	*texture;
+	char	*tmp;
+
+	texture = ft_strdup("");
+	if (!texture)
+		return (ft_putendl_fd("Error Malloc", 2), NULL);
+	while (1)
+	{
+		tmp = get_next_line(fd, 1);
+		if (check_no_texture(tmp) == 1)
+			break ;
+		if (!tmp)
+			return (ft_putendl_fd("Error: No map", 2), NULL);
+		texture = ft_strjoin(texture, tmp);
+		free(tmp);
+	}
+	
 }
 
 char	**take_map(int fd)
